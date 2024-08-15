34058089e5785d1c2e24f1ff32ff4635957fa8de
kelong <klzhang@cnic.cn>
2024-08-15 23:16:23 +0800

    Want to develop Deo/Doe with template specialization; but non-class, non-type partial specialization ‘HisqDslashH<T, EVEN, EVEN>’ is not allowed.

--- HisqDslash.h
+++ HisqDslash.h
@@ -14,7 +14,9 @@
 #include <omp.h>
 #include "Field.h"
 
-// enum EvenOdd_t { EVEN, ODD, FULL };
+// #define Debug
+
+#ifndef Debug
 
 template <typename T>
 void HisqDslash(SpinorField<T> &spinorOut, const SU3Field<T> &U0, const SpinorField<T> &spinorIn)
@@ -32,3 +34,37 @@ void HisqDslash(SpinorField<T> &spinorOut, const SU3Field<T> &U0, const SpinorFi
         }
     }
 }
+
+#else
+
+enum EvenOdd_t { EVEN, ODD, FULL };
+
+template <typename T, EvenOdd_t Leo, EvenOdd_t Reo>
+void HisqDslashH(SpinorField<T> &spinorOut, const SU3Field<T> &U0, const SpinorField<T> &spinorIn);
+
+template <typename T>
+void HisqDslashH<T, EVEN, EVEN>(SpinorField<T> &spinorOut, const SU3Field<T> &U0, const SpinorField<T> &spinorIn)
+{
+#pragma omp parallel for
+    for (int t = 0; t < layout.T; t++) {
+        for (int z = 0; z < layout.Z; z++) {
+            for (int y = 0; y < layout.Y; y++) {
+                for (int x = 0; x < layout.X; x += 1) {
+                    int xf = (x + 1 + layout.X) % layout.X;
+                    int xb = (x - 1 + layout.X) % layout.X;
+                    spinorOut(x, y, z, t) += mul(U0(x, y, z, t), spinorIn(xf, y, z, t)) - mdagv(U0(xb, y, z, t), spinorIn(xb, y, z, t));
+                }
+            }
+        }
+    }
+}
+
+template <typename T>
+void HisqDslash(SpinorField<T> &spinorOut, const SU3Field<T> &U0, const SpinorField<T> &spinorIn)
+{
+    HisqDslashH<T, EVEN, EVEN>(spinorOut, U0, spinorIn);
+}
+
+#endif
+
+#undef Debug
