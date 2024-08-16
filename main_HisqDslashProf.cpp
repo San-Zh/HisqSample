2d8041b0dce6e7fd181696889f04211d22f6477a
kelong <klzhang@cnic.cn>
2024-08-16 16:34:26 +0800

    使用模版特例化实现了 <EVEN,ODD>, <ODD,EVEN>, <FULL,FULL> 三种HisqDslash计算

--- main_HisqDslashProf.cpp
+++ main_HisqDslashProf.cpp
@@ -18,12 +18,6 @@
 #include "timer.h"
 #include "HisqDslash.h"
 
-// #define SINGLE_PREC
-
-#ifndef SIZE
-#define SIZE 4
-#endif
-
 // clang-format off
 using SpinorF = SpinorField<float>;
 using SU3F    = SU3Field<float>;
@@ -47,13 +41,35 @@ void test_HisqDslash(const int numLoop = 50)
 
     // printf("Ready: random spinorIn data before HisqDslash\n");
 
-    t.reset();
-    for (int i = 0; i < numLoop; i++) { HisqDslash(spinorOut, U0, spinorIn); }
-    double time = t.use_sec();
-    double flops = (double) ((6 + 2) * 9 * 2 * numLoop) * (double) layout.vol() * 1.0e-9 / time;
-    double bandwidth = (double) ((3 + 3 + 9) * sizeof(std::complex<float>) * numLoop) * (double) layout.vol() * 1.0e-9 / time;
-    double intensity = flops / bandwidth;
-    printf("Performance: %8.3lf sec, %8.3lf GFLOPS/s, %8.3lf GB/s, %8.3lf flops/byte intensity\n", time, flops, bandwidth, intensity);
+    {
+        t.reset();
+        for (int i = 0; i < numLoop; i++) { HisqDslash<EVEN, ODD>(spinorOut, U0, spinorIn); }
+        double time = t.use_sec();
+        double flops = (double) ((6 + 2) * 9 * 2 * numLoop) * (double) layout.volh() * 1.0e-9 / time;
+        double bandwidth = (double) ((3 + 3 + 9) * sizeof(std::complex<float>) * numLoop) * (double) layout.volh() * 1.0e-9 / time;
+        double intensity = flops / bandwidth;
+        printf("D_{e,o} Performance: %8.3lf sec, %8.3lf GFLOPS/s, %8.3lf GB/s, %8.3lf flops/byte intensity\n", time, flops, bandwidth, intensity);
+    }
+
+    {
+        t.reset();
+        for (int i = 0; i < numLoop; i++) { HisqDslash<EVEN, ODD>(spinorOut, U0, spinorIn); }
+        double time = t.use_sec();
+        double flops = (double) ((6 + 2) * 9 * 2 * numLoop) * (double) layout.volh() * 1.0e-9 / time;
+        double bandwidth = (double) ((3 + 3 + 9) * sizeof(std::complex<float>) * numLoop) * (double) layout.volh() * 1.0e-9 / time;
+        double intensity = flops / bandwidth;
+        printf("D_{o,e} Performance: %8.3lf sec, %8.3lf GFLOPS/s, %8.3lf GB/s, %8.3lf flops/byte intensity\n", time, flops, bandwidth, intensity);
+    }
+
+    {
+        t.reset();
+        for (int i = 0; i < numLoop; i++) { HisqDslash<FULL, FULL>(spinorOut, U0, spinorIn); }
+        double time = t.use_sec();
+        double flops = (double) ((6 + 2) * 9 * 2 * numLoop) * (double) layout.vol() * 1.0e-9 / time;
+        double bandwidth = (double) ((3 + 3 + 9) * sizeof(std::complex<float>) * numLoop) * (double) layout.vol() * 1.0e-9 / time;
+        double intensity = flops / bandwidth;
+        printf("Dslash  Performance: %8.3lf sec, %8.3lf GFLOPS/s, %8.3lf GB/s, %8.3lf flops/byte intensity\n", time, flops, bandwidth, intensity);
+    }
 }
 
 int main(int argc, char const *argv[])
